edb9e7703771241d78a5b0d3b6d438ffd1867cde
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-19 22:31:02 +0200

    Added initial DB

---
+++ Tables/dbo.customer.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[customer]
+(
+[customer_id] [int] NOT NULL IDENTITY(1, 1),
+[first_name] [varchar] (50) COLLATE Latin1_General_CI_AS NOT NULL,
+[last_name] [varchar] (50) COLLATE Latin1_General_CI_AS NOT NULL,
+[age] [tinyint] NOT NULL,
+[zip_code] [varchar] (50) COLLATE Latin1_General_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[customer] ADD CONSTRAINT [PK_customer] PRIMARY KEY CLUSTERED  ([customer_id]) ON [PRIMARY]
+GO
