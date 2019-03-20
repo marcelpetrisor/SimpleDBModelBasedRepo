786f57b82dbd8abf71b98f89943101c1357257e0
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-20 09:56:20 +0200

    Commision_7435  Removed from RC

--- Tables/dbo.commision.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[commision]
-(
-[commision_id] [int] NOT NULL IDENTITY(1, 1),
-[type] [int] NOT NULL,
-[percentage] [tinyint] NOT NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[commision] ADD CONSTRAINT [PK_commision] PRIMARY KEY CLUSTERED  ([commision_id]) ON [PRIMARY]
-GO
