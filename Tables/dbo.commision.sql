6bb8184e0d27cc71f95af2a87d620a0e07fca0d1
Marcel Petrisor <Marcel.Petrisor@wpa.org.uk>
2019-03-20 08:06:34 +0200

    Revert "Add table commision"
    This reverts commit 3990facc06b3df6e7593810f8508f56dad7cec80.

--- Tables/dbo.commision.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[commision]
-(
-[commision_id] [int] NOT NULL,
-[type] [varchar] (50) COLLATE Latin1_General_CI_AS NOT NULL,
-[percentage] [tinyint] NOT NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[commision] ADD CONSTRAINT [PK_commision] PRIMARY KEY CLUSTERED  ([commision_id]) ON [PRIMARY]
-GO
